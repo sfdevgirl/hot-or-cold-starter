0d3f7d8ba65bb68a5f4b97e7b625127502d56990
Shannon Smith <shannones2013@gmail.com>
2015-05-13 23:15:33 -0700

    generated number

--- js/app.js
+++ js/app.js
@@ -1,5 +1,47 @@
+var count = 0;
+var number;
+
+function incrementCounter(){
+	count++; 
+	$('#count').text(count);
+};
+
+function checkGuess(guess){
+	var guessInt = parseInt(guess);
+	var diff = number - guessInt; 
+	if (guess == number){
+		$("#feedback").text("Correct!");
+	} else {
+		console.log("wrong");
+	}
+};
+
+function generateNumber(){
+	number = Math.floor((Math.random() * 100) + 1);
+};
+
+
+// Check high or low
+
+function guessIsHigher(guess){
+	if (guess > number ){
+		return true;
+	} else {
+		return false;
+	}
+
+};
+
+// Compare generated number with user guess
+
+
+// Present appropiate feedback
+
 
 $(document).ready(function(){
+
+	generateNumber();
+	console.log(number);
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -11,6 +53,18 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+  	$("#guessButton").click(function(e){
+  		e.preventDefault();
+  		var guess = $("#userGuess").val();
+  		var guessEl = '<li>' + guess + '</li>';
+  		$('#guessList').append(guessEl);
+  		$('#userGuess').val('');
+  		incrementCounter();
+  		checkGuess(guess);
+  	});
+
+
 
 });
 
